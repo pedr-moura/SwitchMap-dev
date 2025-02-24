832fc9cbdeb9372dc23872629d218b6993818364
Pedro Moura <105287149+pedr-moura@users.noreply.github.com>
2025-02-24 16:39:36 -0300

    Update scriptDados.js

--- frontend/source/script/scriptDados.js
+++ frontend/source/script/scriptDados.js
@@ -1,5 +1,6 @@
 import "./info.js";
 import "./open.js";
+import "./manipularMapa.js";
 
 const toggleMap = document.getElementById('mudartipo')
 const toggleSwView = document.getElementById('toggleShowSwitches')
