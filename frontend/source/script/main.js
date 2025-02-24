23c286c46ee5dcf6dcce0e03c23eec17208f93a0
Pedro Moura <105287149+pedr-moura@users.noreply.github.com>
2025-02-24 16:21:25 -0300

    Update main.js

--- frontend/source/script/main.js
+++ frontend/source/script/main.js
@@ -1,9 +1,9 @@
 // Importando todos os scripts do diretório remoto
-import "https://switch-map-dev-git-main-pedromouras-projects.vercel.app/frontend/source/script/info.js";
-import "https://switch-map-dev-git-main-pedromouras-projects.vercel.app/frontend/source/script/open.js";
-import "https://switch-map-dev-git-main-pedromouras-projects.vercel.app/frontend/source/script/scriptDados.js";
-import "https://switch-map-dev-git-main-pedromouras-projects.vercel.app/frontend/source/script/manipularMapa.js";
-import "https://switch-map-dev-git-main-pedromouras-projects.vercel.app/frontend/source/script/error.js";
-import "https://switch-map-dev-git-main-pedromouras-projects.vercel.app/frontend/source/script/credits.js";
+import "./info.js";
+import "./open.js";
+import "./scriptDados.js";
+import "./manipularMapa.js";
+import "./error.js";
+import "./credits.js";
 
 console.log("Todos os scripts foram carregados com sucesso.");
